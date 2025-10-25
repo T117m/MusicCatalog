9ec47b9dc3edd62454153a9303ae28d8f727e055
T117m <timofey.goryachev@proton.me>
2025-10-26 00:09:13 +0300

    add basic working(?) logic for storage constructor

--- storage/database.go
+++ storage/database.go
@@ -1,11 +1,17 @@
 package storage
 
 import (
-	"path/filepath"
 	"database/sql"
+	"embed"
+	"fmt"
+	"path/filepath"
+	"io/fs"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+//go:embed migrations/*.sql
+var migrations embed.FS
 
 type Storage struct {
 	db *sql.DB
@@ -14,16 +20,27 @@ type Storage struct {
 func NewStorage() (*Storage, error) {
 	dbPath := filepath.Join("storage", "internal", "catalog.db")
 
-	db, err :=  sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error opening a database: %w", err)
 	}
 	defer db.Close()
 
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
 	storage := &Storage{db: db}
 
-	if err := storage.db.Migrate(); err != nil {
-		fmt.Errorf("failed to migrate database: %w", err)
+	// TODO: make an actual migrations pasing
+	q, err := migrations.ReadFile("001_create_tracks.sql")
+	if err != nil {
+		return nil, fmt.Errorf("error reading migration 001: %w", err)
+	}
+
+	_, err = storage.db.Exec(string(q))
+	if err != nil {
+		return nil, fmt.Errorf("error executing migrations: %w", err)
 	}
 
 	return storage, nil
